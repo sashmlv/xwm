620e8caa023221a1e7376ef141d2d7495e4a7a10
sashmlv <sashmlv@yandex.ru>
2019-06-22 20:41:33 +0300

    Add xmobar.

--- .xmonad/xmonad-run.sh
+++ .xmonad/xmonad-run.sh
@@ -4,9 +4,13 @@ compton -b # transparency
 # xcompmgr -n # transparency
 feh --bg-scale /usr/share/backgrounds/wallpaper.jpg # wallpaper
 
-xmodmap initialize
 if [ -s ~/.xmonad/.xmodmap ]; then
+    xmodmap initialize
     xmodmap ~/.xmonad/.xmodmap
+fi
+
+if [ -s ~/.xmonad/.xmobarrc ]; then
+    xmobar ~/.xmonad/.xmobarrc
 fi
 
 xmonad
